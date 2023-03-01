5a5517154656fb99a8ef627b2efc838a22248f30
amrouehab <amrou.ehab@gmail.com>
2023-03-01 13:43:35 +0200

    Update Alg.py
    clean the function and made it more robust and flexible

--- Alg.py
+++ Alg.py
@@ -1,50 +1,68 @@
-# this function takes the sample image and return a bool which indicate whether the blood sample is connected or not
-def is_blood_connected(img_sample):
+import numpy as np
+import cv2
+
+
+def is_blood_connected(img_sample, threshold=145, opening_kernel_size=(4, 4), min_connected_rows=5, min_connected_cols=50):
     # convert the image to gray
     gray = cv2.cvtColor(img_sample, cv2.COLOR_BGR2GRAY)
     # convert it to binary
-    ret, bin_img = cv2.threshold(gray, 145, 255, cv2.THRESH_BINARY)
+    _, bin_img = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
     # setup the kernel (SE) for opening process
-    kernel = np.ones((4, 4), np.uint8)
+    kernel = np.ones(opening_kernel_size, np.uint8)
     # start opening process to reduce the holes and to smooth the image
     img2 = cv2.morphologyEx(~bin_img, cv2.MORPH_OPEN, kernel)
+
     # initializing section (set a default values before entering the loop)
     previous_row_index = 0
-    nof_equal_colms = 0
-    previous_colm = 0
+    nof_equal_cols = 0
+    previous_col = 0
     connected_rows = 0
     row_index = 0
     connected_shape_detected = False
-    # ############ start the loop (which loop around the image point by point to indicate any connected rows ) and
-    # the main idea is by detecting for every row any connected pixels in a single row if there are more than 50
-    # continuous points which have a value of 255 this means that this row has high probability to be part of a
-    # connected blood so after that increase the connected rows by 1 then do this for all rows until the nof
-    # connected rows be >4 then now we have more than 4 continuous rows which have more than 50 continuous connected
-    # pixels then break the loop and return a bool which indicate that the blood is connected
 
-    for row in img2:  # get the current row from the outer loop
-        for col_val in row:  # loop around this row
-            if col_val == 255:  # check if the current col_value is equal 255
-                if previous_colm == 255:  # check also if the prev colum value is 255
-                    nof_equal_colms = nof_equal_colms + 1  # if true increase nof connected continuous columns by 1
-                previous_colm = 255  # set the previous column value to be 255 cuz the current column is 255 and will be a previous one
-            else:  # if the currnet column value not 255 then set the prev_colum value to be 0
-                previous_colm = 0
-                if nof_equal_colms < 50:  # check if the nof connected columns for the current row is more than 50
-                    # if so reset the counter because the current column value is zero which breaks the continuity of
-                    #  the connected columns
-                    nof_equal_colms = 0
-                else:  # if nof connected columns >255 break the loop because we have got a connected row
+    # loop over each row of the image
+    for row in img2:
+        # reset the number of connected columns for this row
+        nof_equal_cols = 0
+        # loop over each column in this row
+        for col_val in row:
+            # check if the current column value is equal 255
+            if col_val == 255:
+                # check if the prev column value is also 255
+                if previous_col == 255:
+                    # if true increase nof connected continuous columns by 1
+                    nof_equal_cols += 1
+                # set the previous column value to 255
+                previous_col = 255
+            else:
+                # if the current column value is not 255, set the previous column value to 0
+                previous_col = 0
+                # if the number of connected columns for the current row is less than the minimum required,
+                # reset the counter because the current column value is zero which breaks the continuity of
+                # the connected columns
+                if nof_equal_cols < min_connected_cols:
+                    nof_equal_cols = 0
+                # if the number of connected columns is greater than or equal to the minimum required,
+                # break the loop because we have a connected row
+                else:
                     break
 
-        if nof_equal_colms > 0:  # check if the current row is have  connected columns
-            if row_index - previous_row_index == 1:  # if the current connected row index is just after the prev connected row
-                connected_rows = connected_rows + 1  # increase connected rows by 1
+        # check if the current row has connected columns
+        if nof_equal_cols > 0:
+            # if the current connected row index is just after the previous connected row index,
+            # increase the number of connected rows
+            if row_index - previous_row_index == 1:
+                connected_rows += 1
+            # set the current row index to be the previous row index for next time
+            previous_row_index = row_index
 
-            previous_row_index = row_index  # set the current row index to be a previous row for next time
-        if connected_rows > 4:  # check if the detected connected rows is more than 4 to break the loop
+        # check if the number of connected rows is greater than or equal to the minimum required,
+        # set the connected_shape_detected flag and break the loop
+        if connected_rows >= min_connected_rows:
             connected_shape_detected = True
             break
-        row_index = row_index + 1  # increment the row index
+
+        # increment the row index
+        row_index += 1
 
     return connected_shape_detected, ~bin_img
